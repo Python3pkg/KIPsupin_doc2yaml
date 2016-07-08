72006510028f368f9f949ba5649083470f41ebf0
薛丞宏 <ihcaoe@gmail.com>
2016-07-09 07:10:15 +0800

    無結構化，應該號做array

--- 詞頻doc/轉換doc.py
+++ 詞頻doc/轉換doc.py
@@ -4,25 +4,30 @@ from bs4 import BeautifulSoup
 
 
 class 轉換doc:
+
     def __init__(self, 檔名):
-        self.檔名=檔名
+        self.檔名 = 檔名
+
     def doc轉html(self):
         程序 = Popen(['wvWare', self.檔名], stdin=None, stdout=PIPE, stderr=PIPE)
         輸出資訊, _錯誤輸出資訊 = 程序.communicate()
-        self.html=輸出資訊.decode('utf-8')
+        self.html = 輸出資訊.decode('utf-8')
         return self
+
     def 提html(self):
         return self.html
-    def html轉json(self):
-        bs=BeautifulSoup(self.html, "lxml")
-        table=bs.table
-        全部資料=[]
+
+    def html轉array(self):
+        bs = BeautifulSoup(self.html, "lxml")
+        table = bs.table
+        全部資料 = []
         for tr in table.find_all('tr'):
-            一逝資料=[]
+            一逝資料 = []
             for td in tr.find_all('td'):
-                一逝資料.append(td.get_text("",strip=True))
+                一逝資料.append(td.get_text("", strip=True))
             全部資料.append(一逝資料)
-        self.json=全部資料
+        self.array = 全部資料
         return self
-    def 提json(self):
-        return self.json
+
+    def 提array(self):
+        return self.array
--- 試驗/testHtml轉array試驗.py
+++ 試驗/testHtml轉array試驗.py
@@ -3,23 +3,23 @@ from 詞頻doc.轉換doc import 轉換doc
 from os.path import dirname, abspath, join
 
 
-class html轉json試驗(TestCase):
+class html轉array試驗(TestCase):
 
     def 檔案(self, 檔名):
         return join(dirname(abspath(__file__)), '檔案', 檔名)
 
     def test_有分類(self):
-        json = 轉換doc(self.檔案('57-火燒紅蓮寺歌.doc')).doc轉html().html轉json().提json()
-        self.assertIn(['文類', '歌仔冊'], json)
+        array = 轉換doc(self.檔案('57-火燒紅蓮寺歌.doc')).doc轉html().html轉array().提array()
+        self.assertIn(['文類', '歌仔冊'], array)
 
     def test_有段(self):
-        json = 轉換doc(self.檔案('57-火燒紅蓮寺歌.doc')).doc轉html().html轉json().提json()
+        array = 轉換doc(self.檔案('57-火燒紅蓮寺歌.doc')).doc轉html().html轉array().提array()
         self.assertIn([
             '段',
             '下集工夫即卜展，兩平五路請劍仙，[門@丨]着紅姑即無變，桂武逍遙力做前。',
             'E7-chip8 kang-hu chiah beh tian2, nng7-peng5 ngou2-lou7 chhiann2 kiam3-sian, tu2 tioh8 Ang5-kou chiah bo5 pian3, Kui3-bu2 Siau-iau5 liah8 cho3-chian5.'
-        ], json)
+        ], array)
 
     def test_數量著(self):
-        json = 轉換doc(self.檔案('57-火燒紅蓮寺歌.doc')).doc轉html().html轉json().提json()
-        self.assertEqual(len(json), 295)
+        array = 轉換doc(self.檔案('57-火燒紅蓮寺歌.doc')).doc轉html().html轉array().提array()
+        self.assertEqual(len(array), 295)
